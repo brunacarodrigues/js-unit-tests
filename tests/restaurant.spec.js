8dba4cb8694928b05b31d320853212a5f6c0b8d7
brunacarodrigues <brunacris.rodrigues@gmail.com>
2022-09-21 12:20:14 -0300

    requisito 8 e 9

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -54,7 +54,6 @@ const createMenu = require('../src/restaurant');
 
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // TESTE 1: Verifique se função `createMenu()` retorna um objeto que possui a chave `fetchMenu`, a qual tem como valor uma função.
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
