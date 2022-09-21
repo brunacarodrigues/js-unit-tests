132047d11a9c760eeb0837e75cd6f47860a7835b
brunacarodrigues <brunacris.rodrigues@gmail.com>
2022-09-21 14:42:30 -0300

    recorrigindo requisitos 2, 3 e 4

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -2,7 +2,6 @@
 /* eslint-disable no-unused-vars */
 
 const numbers = require('../src/numbers');
-const assert = require('assert');
 
 /*
   A função `numbers` recebe um array de tamanho variável e retorna `true` se todos os parâmetros forem do tipo 'number' e `false` caso contrário.
@@ -15,15 +14,15 @@ const assert = require('assert');
 
 */
 
-describe('#numbers', () => {
+describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    expect(numbers([1, 2, 3, 4, 5])).toEqual(true);
+    expect(numbers([1, 2, 3, 4, 5])).toBe(true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    expect(numbers([1, 2, "3", 4, 5])).toEqual(false);
+    expect(numbers([1, 2, "3", 4, 5])).toBeBe(false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    expect(numbers([1, "a", 3])).toEqual(false);
+    expect(numbers([1, "a", 3])).toBe(false);
     // Escreva um teste em que a função recebe [' '] e retorna false
-    expect(numbers([" "])).toEqual(false);
+    expect(numbers([" "])).toBe(false);
   });
 });
